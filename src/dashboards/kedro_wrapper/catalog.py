1c2b003e16e7485631e137fb755e1b9d41967218
ejwillemse <ejwillemse@gmail.com>
2023-03-22 18:31:54 +0000

    perf: addde auto context reload option when saving file
    much more stable option as saving is usually done when there are edits (with keys) , or with manual file uploads.

--- src/dashboards/kedro_wrapper/catalog.py
+++ src/dashboards/kedro_wrapper/catalog.py
@@ -7,6 +7,8 @@ import pandas as pd  # noqa: I201
 import streamlit as st  # noqa: I201
 from kedro.io import DataSetError
 
+from dashboards.kedro_wrapper import context
+
 logger = logging.getLogger(__name__)
 
 
@@ -57,9 +59,13 @@ def save(
     dataset: str,
     data: tp.Any,
     key: str | None = None,
+    reload_catalog: bool = True,
 ):
+    "It will be needed to reload the catalog after saving files, especially partitioned files."
     catalog = st.session_state["kedro"]["catalog"]
     if key is not None:
         data = {key: data}
     data = catalog.save(dataset, data)
     logger.info(f"Saved data: `{dataset}` with key `{key}`")
+    if reload_catalog:
+        context.reload_catalog()
